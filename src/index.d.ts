af8de6c0d58d0cf00d60f57341af6b76cc7d5043
Raffaele Filiberti <raffaele@activetheory.net>
2025-01-21 18:24:54 +0100

    chore(docs): fix types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,25 +1,8 @@
-export type FitTextResult = {
-  fontSize: number;
-  text: string;
-};
-
-export type FitTextOptions = {
+export type BalanceTextOptions = {
   el: HTMLElement;
-  box?: HTMLElement;
-  maxWidth?: number | null;
-  maxHeight?: number | null;
-  minFontSize?: number;
-  debug?: boolean;
-  flip?: boolean;
-  clipOnly?: boolean;
-  singleLine?: boolean;
-  boxWidthOnly?: boolean;
-  boxMultiplier?: [number, number];
-  clip?: string;
-  htmlClip?: string;
-  afterFit?: (result: FitTextResult) => void;
-  maxWidthLimit?: number;
-  maxHeightLimit?: number;
+  ratio: number;
+  useParent: boolean;
+  debug: boolean;
 };
 
-export default function fitText(options: FitTextOptions): FitTextResult;
+export default function balanceText(options: BalanceTextOptions): void;
